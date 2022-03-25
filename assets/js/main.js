005674673be3f8daa0246fc9207cdf57b590a78e
aciernoantonio <aciernoantonio.int@gmail.com>
2022-03-25 16:19:08 +0100

    add name km and age function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 function myFunction() {
-    let user_name = document.getElementById("full_name").value;
-    document.getElementById("result").innerHTML = user_name;
+    let full_name = document.getElementById("full_name").value;
+    document.getElementById("selected_name").innerHTML = full_name;
+
+    let km = document.getElementById("km").value;
+    document.getElementById("selected_km").innerHTML = km;
+
+    let age = document.getElementById("age").value;
+    document.getElementById("selected_age").innerHTML = age;
 }
 
 
