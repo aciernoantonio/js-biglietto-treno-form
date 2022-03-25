bb5ffa85910a368c08c17e277cabc5309b05ebf6
aciernoantonio <aciernoantonio.int@gmail.com>
2022-03-25 17:49:30 +0100

    add carrozza number onclick

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,14 @@ function myFunction() {
     let age = document.getElementById("age").value;
     document.getElementById("selected_age").innerHTML = age;
 
+    let n_carrozza = document.getElementById("n_carrozza").innerHTML = Math.floor(Math.random() * 10) +1;
+
 }
+
+//display carrozza
+
+
+
 
 //costo per km e calcolo prezzo
 const km_price = 0.21;
@@ -15,12 +22,13 @@ const ticket_price = km * km_price;
 const minors_discount = ticket_price - (ticket_price * 20 / 100);
 const over_discount = ticket_price - (ticket_price * 40 / 100);
 
-if (age === `Sconto Minorenne`) {
+if (age === `Minorenne`) {
     minors_discount;
-    document.getElementById("full_price").innerHTML = minors_discount
+    
 } else if (age === `Sconto Over 65`) {
     over_discount;
+
 } else if (age === `Nessuno`) {
-    ticket_price
+    ticket_price;
 }
 
